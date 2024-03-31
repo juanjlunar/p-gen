f9d6224346a9d2b88fae9337f249058ac4ddd88e
Juan Lunar <juanjlunarc@gmail.com>
2024-03-31 19:08:20 -0400

    style: remove commented code

--- cli/src/app.module.ts
+++ cli/src/app.module.ts
@@ -4,9 +4,6 @@ import { CosmiconfigModule } from './cosmiconfig/cosmiconfig.module';
 import { UtilsModule } from './utils/utils.module';
 import { LoggerModule } from './logger/logger.module';
 import { HasuraModule } from './hasura/hasura.module';
-// import { KnexModule } from './knex/knex.module';
-// import { CONFIG_OPTIONS_TOKEN } from './cosmiconfig/constants/injection-tokens.constant';
-// import { Config } from './common/types';
 
 @Module({
   imports: [
@@ -15,38 +12,6 @@ import { HasuraModule } from './hasura/hasura.module';
     UtilsModule,
     LoggerModule,
     HasuraModule,
-    // KnexModule.registerAsync({
-    //   inject: [CONFIG_OPTIONS_TOKEN],
-    //   useFactory: async (configOptions: Config) => {
-    //     // Return dummy connection if the export type is JSON.
-    //     if (
-    //       !configOptions?.exports?.type ||
-    //       configOptions?.exports?.type === 'JSON'
-    //     ) {
-    //       return {
-    //         client: 'pg',
-    //         connection: {
-    //           connectionString: '',
-    //         },
-    //       };
-    //     }
-
-    //     const dbOptions = configOptions?.exports?.options;
-
-    //     /**
-    //      * If the export type is DB and the options are missing we throw an exception to avoid initializing this module.
-    //      *
-    //      */
-    //     if (configOptions?.exports?.type === 'DB' && !dbOptions) {
-    //       throw new Error('Missing DB export options.');
-    //     }
-
-    //     return {
-    //       client: dbOptions.client,
-    //       connection: dbOptions.connection,
-    //     };
-    //   },
-    // }),
   ],
   controllers: [],
   providers: [],
