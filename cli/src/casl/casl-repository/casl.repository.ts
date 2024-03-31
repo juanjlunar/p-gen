aa2101715bb783f9d835afcefdd425164fc56204
Juan Lunar <juanjlunarc@gmail.com>
2024-03-31 19:03:48 -0400

    style: remove unused repository class

--- cli/src/casl/casl-repository/casl.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ICaslRepository } from './icasl-repository.interface';
-
-@Injectable()
-export class CaslRepository implements ICaslRepository {}
--- cli/src/casl/casl.module.ts
+++ cli/src/casl/casl.module.ts
@@ -3,21 +3,11 @@ import { CaslGeneratorCommand } from './casl-generator-command';
 import { CaslService } from './casl.service';
 import { CosmiconfigModule } from '../cosmiconfig/cosmiconfig.module';
 import { HasuraModule } from '../hasura/hasura.module';
-import { ICaslRepository } from './casl-repository/icasl-repository.interface';
-import { CaslRepository } from './casl-repository/casl.repository';
 import { CaslPermissionTransformer } from './casl-permission-transformer/casl-permission-transformer';
 
 @Module({
   imports: [CosmiconfigModule, HasuraModule],
-  providers: [
-    {
-      provide: ICaslRepository,
-      useClass: CaslRepository,
-    },
-    CaslPermissionTransformer,
-    CaslGeneratorCommand,
-    CaslService,
-  ],
+  providers: [CaslPermissionTransformer, CaslGeneratorCommand, CaslService],
   exports: [CaslPermissionTransformer, CaslService],
 })
 export class CaslModule {}
